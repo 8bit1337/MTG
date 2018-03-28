d9660e8c08114b09dec925518f482de380b93146
Jason Gruetzmacher <8bit1337@gmail.com>
2018-03-28 16:17:39 -0400

    20180328-1617

--- deck-shuffle.py
+++ deck-shuffle.py
@@ -10,35 +10,33 @@ from csv import DictReader
 from random import shuffle
 
 def main(argv):
-  file=open('Commander\Slivers.csv', newline='')
+  file=open('Commander\Slivers!.csv', newline='')
   allrows = DictReader(file)
+  
   deck=[]
-  print('Importing Deck...')
+  
   for row in allrows:
     i=0
     while i<int(row['Count']):
       if 'Creature' in row['Type']:
         typ='Creature'
+      elif 'Land' in row['Type']:
+        typ='Land'
       else:
         typ=row['Type']
       deck.append(typ+': '+row['Name'])
       i+=1
   
-  print('Done') 
-  
-  draw=1
-  while draw<=3:
-    print('Draw: ')  
-    shuffle(deck)
-    for i in range (0, 7):
-      print('   '+deck[i])
-    print('   -----')
-    for i in range(7, 10):
-      print('   '+deck[i])
-    draw+=1
-    print('')  
-  
   file.close()
+  
+  shuffle(deck)
+  for i in range (0,10):
+    for j in range (0, 10):
+      print('   '+deck[10*i+j])
+    print('   -----')
+  print('')  
+  
+  
   return True
 
 if __name__ == '__main__':
